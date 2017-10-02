6615780c6101048f47b16b67316568e7fb82b742
David Reid <mackron@gmail.com>
2017-10-02 17:40:20 +1000

    Add argon2-lib.c which should implement the whole argon2 library.
    This change allows applications to integrate argon2 by simply adding
    this new file to their source tree rather than compiling it as a
    separate library. Why do project maintainers not see the value in
    this?!

---
+++ src/argon2-lib.c
@@ -0,0 +1,16 @@
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable:4804)
+#endif
+
+#include "../external/argon2/src/blake2/blake2b.c"
+#include "../external/argon2/src/argon2.c"
+#include "../external/argon2/src/core.c"
+#include "../external/argon2/src/encoding.c"
+#include "../external/argon2/src/thread.c"
+#include "../external/argon2/src/opt.c"
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
